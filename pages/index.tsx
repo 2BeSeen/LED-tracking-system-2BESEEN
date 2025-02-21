aaafae28126bdbdc5bffa4c553cb4d2c82ff757f
2BeSeen <henrydevos20@gmail.com>
2025-02-21 14:44:07 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1>2BeSeen Werkuren Tracking Systeem</h1>
       <p>Welkom bij het werkuren tracking systeem.</p>
+      <nav>
+        <ul>
+          <li><Link href="/employee/dashboard">Werknemer Dashboard</Link></li>
+          <li><Link href="/employer/dashboard">Werkgever Dashboard</Link></li>
+        </ul>
+      </nav>
     </div>
   )
 }
