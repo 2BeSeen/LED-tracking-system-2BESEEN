7738dfb046e89654ba6b759f5549905154795b78
2BeSeen <henrydevos20@gmail.com>
2025-02-21 14:33:59 +0100

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>2BeSeen Werkuren Tracking Systeem</h1>
+      <p>Welkom bij het werkuren tracking systeem.</p>
+    </div>
+  )
+}
+
+export default Home
